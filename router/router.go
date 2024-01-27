1f370181943194fa843286612c21d4083e56b83b
RichardPMachado <richardmachado2211@gmail.com>
2024-01-26 22:45:07 -0300

    feat: add database handler

--- router/router.go
+++ router/router.go
@@ -1,12 +1,19 @@
 package router
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func Initialize() {
 	//Initialize Router
 	router := gin.Default()
 	// Initialize Routes
 	initializeRoutes(router)
+
+	// port := os.Getenv("PORT")
+	// if port != "" {
+	// 	port = ":8080"
+	// }
 	// Run the Server
 	router.Run(":8080") // listen and Serve on 0.0.0.0:8080
 }
